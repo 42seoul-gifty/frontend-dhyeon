5d7729e23fe0395a29862fb5e81f1668f1ade948
som-syom <dhyeon@student.42seoul.kr>
2021-08-29 02:15:47 +0900

    Add - token 발급

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import KakaoLoginImg from "../images/kakao_login.png";
 
+
 const REST_API_KEY = "82bc00a056d39582ec6d9aef218f5927"
 const REDIRECT_URI = "http://localhost:3000/login"
 function Home() {
@@ -15,4 +16,5 @@ function Home() {
 	);
 }
 
+export {REDIRECT_URI, REST_API_KEY};
 export default Home;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,78 @@
-import React from "react";
-import queryString from "query-string";
+import React, { useEffect } from "react";
+// import axios from "axios";
+import {REDIRECT_URI, REST_API_KEY} from "./Home";
 
 function Login({location, history}) {
-	const {code} = queryString.parse(location.search);
+	const code = new URLSearchParams(window.location.search).get('code');
 
+	const postData = {
+		grant_type: 'authorization_code',
+		client_id: REST_API_KEY,
+		redirect_uri: REDIRECT_URI,
+		code: code,
+		client_secret: 'LNKXo86CL50cISuRvu3zQKexYwQbfaVQ'
+	};
+
+	// let axiosConfig = {
+	// 	headers: {
+	// 		'Content-Type': 'application/x-www-form-urlencoded;',
+	// 	}
+	//   };
+
+	// async function getToken() {
+	// 	try {
+	// 		const response = await axios.post(
+	// 			'https://kauth.kakao.com/oauth/token', postData, axiosConfig
+	// 		).then(() => {
+	// 			console.log(response);
+	// 		})
+	// 	} catch (error) {
+	// 		console.log("error");
+	// 		history.push('/');
+	// 	}
+	// }
+
+	// useEffect(() =>{
+	// 	const code = window.location.search.split("=")[1];
+	// 	const postData = {
+	// 		grant_type: "authorization_code",
+	// 		client_id: REST_API_KEY,
+	// 		redirect_uri: REDIRECT_URI,
+	// 		code: code,
+	// 	};
+
+	// 	fetch("https://kauth.kakao.com/oauth/token", {
+	// 		method: "POST",
+	// 		headers: [
+	// 			['Content-Type', 'application/x-www-form-urlencoded;charset=utf-8'],
+	// 		],
+	// 		body: JSON.stringify(postData)
+	// 	})
+	// 	.then(res => res.json())
+	// 	.then((data) => {
+	// 		console.log(data)
+	// 	})
+	// }, [])
+	
+	useEffect(() => {
+		const reqOpt = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			// body: postData
+		};
+
+		const params = new URLSearchParams(postData).toString();
+
+		fetch(`https://kauth.kakao.com/oauth/token?${params}`, reqOpt)
+		.then(response => response.json())
+		.then(data => console.log(data))
+	}, [])
+	
 	return (
 		<>
 			Login
 			{console.log(code)}
 			로그인 성공
-			{history.push('/')}
 		</>
 	);
 }

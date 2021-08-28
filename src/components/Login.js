77adab0036e90d549b467497f1a8f6db06c99a98
som-syom <dhyeon@student.42seoul.kr>
2021-08-29 04:03:18 +0900

    Style - 주석 제거

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-// import axios from "axios";
 import {REDIRECT_URI, REST_API_KEY} from "./Home";
 
 function Login({location, history}) {
@@ -12,53 +11,11 @@ function Login({location, history}) {
 		code: code,
 		client_secret: 'LNKXo86CL50cISuRvu3zQKexYwQbfaVQ'
 	};
-
-	// let axiosConfig = {
-	// 	headers: {
-	// 		'Content-Type': 'application/x-www-form-urlencoded;',
-	// 	}
-	//   };
-
-	// async function getToken() {
-	// 	try {
-	// 		const response = await axios.post(
-	// 			'https://kauth.kakao.com/oauth/token', postData, axiosConfig
-	// 		).then(() => {
-	// 			console.log(response);
-	// 		})
-	// 	} catch (error) {
-	// 		console.log("error");
-	// 		history.push('/');
-	// 	}
-	// }
-
-	// useEffect(() =>{
-	// 	const code = window.location.search.split("=")[1];
-	// 	const postData = {
-	// 		grant_type: "authorization_code",
-	// 		client_id: REST_API_KEY,
-	// 		redirect_uri: REDIRECT_URI,
-	// 		code: code,
-	// 	};
-
-	// 	fetch("https://kauth.kakao.com/oauth/token", {
-	// 		method: "POST",
-	// 		headers: [
-	// 			['Content-Type', 'application/x-www-form-urlencoded;charset=utf-8'],
-	// 		],
-	// 		body: JSON.stringify(postData)
-	// 	})
-	// 	.then(res => res.json())
-	// 	.then((data) => {
-	// 		console.log(data)
-	// 	})
-	// }, [])
 	
 	useEffect(() => {
 		const reqOpt = {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			// body: postData
 		};
 
 		const params = new URLSearchParams(postData).toString();
